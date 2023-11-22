2bda5824b781a58ef96f49dbba05d266f247e31d
eddie wang <wang8119@gmail.com>
2023-11-23 02:07:52 +0800

    profile post

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -4,7 +4,28 @@ const passport = require("passport");
 const Profile = require("../../models/Profile");
 
 router.get("/test", (req, res) => {
-  const { email, name, password, identity } = req.body;
+  const { type, describe, income, expend, cash, remark } = req.body;
 });
+
+router.post(
+  "/add",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { type, describe, income, expend, cash, remark } = req.body;
+
+    const profileFields = {};
+    if (type) profileFields.type = type;
+    if (describe) profileFields.type = describe;
+    if (income) profileFields.type = income;
+    if (expend) profileFields.type = expend;
+    if (cash) profileFields.type = cash;
+    if (remark) profileFields.type = remark;
+
+    new Profile(profileFields)
+      .save()
+      .then((profile) => res.json(profile))
+      .catch((err) => console.log(err));
+  }
+);
 
 module.exports = router;
