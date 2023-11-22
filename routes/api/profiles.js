5c380627fbc72669e6eced33757fdf5c89bc37e4
eddie wang <wang8119@gmail.com>
2023-11-23 02:25:30 +0800

    Profile.get

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -28,4 +28,34 @@ router.post(
   }
 );
 
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.find()
+      .then((profile) => {
+        if (!profile) {
+          return res.status(404).json("沒有任何內容");
+        }
+        res.json(profile);
+      })
+      .catch((err) => res.status(404).json(err));
+  }
+);
+
+router.get(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ _id: req.params.id })
+      .then((profile) => {
+        if (!profile) {
+          return res.status(404).json("沒有任何內容");
+        }
+        res.json(profile);
+      })
+      .catch((err) => res.status(404).json(err));
+  }
+);
+
 module.exports = router;
